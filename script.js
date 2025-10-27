fb425b0512c1d2b6cd72f213a45f390ad8f60762
hallow.ween <sky608479@gmail.com>
2025-10-26 20:28:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const redS =    
+  document.getElementById("RedSlider");
+const blueS = 
+  document.getElementById("BlueSlider");
+const greenS = 
+  document.getElementById("GreenSlider");
 
+const redVal =
+  document.getElementById("RedV");
+const blueVal =
+  document.getElementById("BlueV");
+const greenVal =
+  document.getElementById("GreenV");
+
+const colorBox =
+  document.getElementById("ColorBox");
+
+function updateColor() {
+const Red = 
+  redS.value;
+const Blue =
+  blueS.value;
+const Green =
+  greenS.value;
+
+const updatedColor =
+  `rgb(${Red}, ${Green}, ${Blue})`;
+
+colorBox.style.backgroundColor =
+  updatedColor;
+
+RedVal.textContent = Red;
+GreenVal.textContent = Green;
+BlueVal.textContent = Blue;
+
+}
+
+redS.addEventListener("input", updateColor);
+greenS.addEventListener("input", updateColor);
+blueS.addEventListener("input", updateColor);
+
+updateColor();
