502420d6637925019d5185f0c59e24e8985a9106
hallow.ween <sky608479@gmail.com>
2025-11-02 15:11:02 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -213,3 +213,48 @@ if (currentImg > 0) {
 }
 }
                           
+const redCS =    
+  document.getElementById("RColorSlider");
+const blueCS = 
+  document.getElementById("BColorSlider");
+const greenCS = 
+  document.getElementById("GColorSlider");
+
+const redValC =
+  document.getElementById("redC");
+const blueValC =
+  document.getElementById("blueC");
+const greenValC =
+  document.getElementById("greenC");
+  
+const rgb =
+  document.getElementById("rgb");
+
+
+function showColor() {
+	
+const r = redCS.value;
+const b = blueCS.value;
+const g = greenCS.value;
+
+let rr = Math.round((r / 9 ) * 255);
+let bb = Math.round((b / 9 )* 255);
+let gg = Math.round((g / 9 )* 255);
+
+const colorCode =
+`rgb(${rr}, ${gg}, ${bb})`;
+rgb.textContent = colorCode;
+
+redValC.textContent = "Red: " + r;
+greenValC.textContent = "Green: " + g;
+blueValC.textContent = "Blue: " + b;
+
+
+
+}
+
+redCS.addEventListener("input", showColor);
+greenCS.addEventListener("input", showColor);
+blueCS.addEventListener("input", showColor);
+
+showColor();
